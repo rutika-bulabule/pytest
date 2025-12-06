41cc98791a8a53695ea7956eb5cbfb39c236163e
rutika-bulabule <rutikabulabule@gmail.com>
2025-12-06 11:16:03 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,6 +1,8 @@
-def add(a,b):
-  return a+b
-if _name=="main_":
-  x=10
-  y=20
-  print("Sum:",add(x,y))
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = 10
+    y = 20
+    print("Sum:", add(x, y))
+
